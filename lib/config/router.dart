554ea604b6bcd6f2ff8e6e6de49be8b434791bf0
Aschwin Bruyning <aschwinbruyning@outlook.com>
2026-05-02 21:59:00 +0200

    style: dart format router.dart

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -6,18 +6,15 @@ class AtelierRouter {
   const AtelierRouter._();
 
   static GoRouter build() => GoRouter(
-        routes: [
-          GoRoute(
-            path: '/',
-            builder: (_, __) => const HomeContainer(),
-          ),
-          GoRoute(
-            path: '/pocket/:id',
-            builder: (_, state) {
-              final id = state.pathParameters['id']!;
-              return DetailContainer(goalCategoryId: id);
-            },
-          ),
-        ],
-      );
+    routes: [
+      GoRoute(path: '/', builder: (_, __) => const HomeContainer()),
+      GoRoute(
+        path: '/pocket/:id',
+        builder: (_, state) {
+          final id = state.pathParameters['id']!;
+          return DetailContainer(goalCategoryId: id);
+        },
+      ),
+    ],
+  );
 }

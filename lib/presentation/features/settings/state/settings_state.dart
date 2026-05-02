397d845b2f9787ab92e909f39404012393852f45
Aschwin Bruyning <aschwinbruyning@outlook.com>
2026-05-02 21:46:57 +0200

    style: apply dart format to settings_state.dart

--- lib/presentation/features/settings/state/settings_state.dart
+++ lib/presentation/features/settings/state/settings_state.dart
@@ -2,15 +2,19 @@ import 'package:atelier/domain/models/app_settings.dart';
 import 'package:equatable/equatable.dart';
 
 class SettingsState extends Equatable {
-  const SettingsState({this.settings = const AppSettings(), this.loaded = false});
+  const SettingsState({
+    this.settings = const AppSettings(),
+    this.loaded = false,
+  });
 
   final AppSettings settings;
   final bool loaded;
 
-  SettingsState copyWith({AppSettings? settings, bool? loaded}) => SettingsState(
-    settings: settings ?? this.settings,
-    loaded: loaded ?? this.loaded,
-  );
+  SettingsState copyWith({AppSettings? settings, bool? loaded}) =>
+      SettingsState(
+        settings: settings ?? this.settings,
+        loaded: loaded ?? this.loaded,
+      );
 
   @override
   List<Object?> get props => [settings, loaded];

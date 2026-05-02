3488414b73be58286ff7ed0e142949bb638b7e7c
Aschwin Bruyning <aschwinbruyning@outlook.com>
2026-05-02 21:21:09 +0200

    style: apply dart format to lib/domain/models/goal_category.dart

--- lib/domain/models/goal_category.dart
+++ lib/domain/models/goal_category.dart
@@ -13,13 +13,17 @@ class GoalCategory extends Equatable {
   final int order;
   final bool isAddSlot;
 
-  GoalCategory copyWith({String? id, String? name, int? order, bool? isAddSlot}) =>
-      GoalCategory(
-        id: id ?? this.id,
-        name: name ?? this.name,
-        order: order ?? this.order,
-        isAddSlot: isAddSlot ?? this.isAddSlot,
-      );
+  GoalCategory copyWith({
+    String? id,
+    String? name,
+    int? order,
+    bool? isAddSlot,
+  }) => GoalCategory(
+    id: id ?? this.id,
+    name: name ?? this.name,
+    order: order ?? this.order,
+    isAddSlot: isAddSlot ?? this.isAddSlot,
+  );
 
   @override
   List<Object?> get props => [id, name, order, isAddSlot];
